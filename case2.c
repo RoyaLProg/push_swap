fd12b17ca0c55eeb54664b787c4967ca91981434
Camille CAMBIUM <ccambium@student.42.fr>
2022-04-26 15:18:24 +0200

    case 2 numbers

--- case2.c
+++ case2.c
@@ -1,32 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   case_less100.c                                     :+:      :+:    :+:   */
+/*   case2.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ccambium <ccambium@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/07 03:01:05 by ccambium          #+#    #+#             */
-/*   Updated: 2022/04/09 13:43:03 by ccambium         ###   ########.fr       */
+/*   Created: 2022/04/26 14:37:44 by ccambium          #+#    #+#             */
+/*   Updated: 2022/04/26 14:41:33 by ccambium         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include <stdio.h>
 
-void	case_less100(t_stack *stackA, t_stack *stackB)
+void	case2(t_stack *stackA, t_stack *stackB)
 {
-	while (!is_sorted(stackA, stackB))
-	{
-		if (stackA->tab[0] > stackA->tab[1])
-		{
-			if (stackA->tab[0] > stackA->tab[2])
-				push_sa(stackA);
-			else
-				push_ra(stackA);
-		}
-		else if (stackA->tab[1] > stackA->tab[2])
-			push_sa(stackA);
-		else
-			push_ra(stackA);
-	}
+	if (stackA->tab[0] > stackA->tab[1])
+		push_sa(stackA);
 }
